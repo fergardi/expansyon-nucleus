8ea014be9cfdc037c2699d9e373a27fd4e028cea
TECNOSYLVA\fgarcia <fernando.garcia@xeridia.com>
2017-03-15 16:58:59 +0100

    FIxed require background image.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -129,7 +129,7 @@ router.beforeEach((to, from, next) => {
     if (document.getElementById('scroll')) document.getElementById('scroll').scrollIntoView(true)
     // Main.close('left')
     // Main.close('right')
-    if ((security.indexOf(to.name) !== -1) && !vuex.state.user.logged) {
+    if ((security.indexOf(to.name) !== -1) && !vuex.state.player.logged) {
       router.push({ path: '/login' })
     } else {
       return next()
